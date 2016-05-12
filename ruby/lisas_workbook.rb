2f1da27f6d23e129e5f4b043a369bd30815b07fc
Joe Attardi <joe@attardi.net>
2016-05-12 18:21:04 -0400

    Solution for Lisa's Workbook

--- ruby/lisas_workbook.rb
+++ ruby/lisas_workbook.rb
@@ -1,3 +1,4 @@
+# https://www.hackerrank.com/challenges/bear-and-workbook
 def read_numbers
   gets.chomp.split(' ').map { |num| num.to_i }
 end
@@ -10,19 +11,20 @@ specials = 0
 chapter = 1
 page = 1
 problem = 1
+
 while chapter <= num_chapters
   problem = 1
   while problem <= chapter_problems[chapter - 1]
-    puts "Page #{page}, Chapter #{chapter}, Problem #{problem}"
+
     specials += 1 if problem == page
-    problem += 1
-    if problem % (page_problems) == 0
+
+    if problem % (page_problems) == 0 || problem == chapter_problems[chapter - 1]
       page += 1
     end
+
+    problem += 1
   end
   chapter += 1
-  page += 1
-  puts ""
 end
 
 puts specials
